64c9ab2e185ec3a7830f3c0c331e5d84d5974656
Berkay Yılmaz <berkayyilmaz@Berkay-MacBook-Air.local>
2024-09-01 18:11:10 +0300

    Rating component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Container } from "react-bootstrap";
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <>
-      <Header />
+      <Header / >
       <main className='py-3'>
         <Container>
-          <HomePage/>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,13 +1,33 @@
-import { Card } from 'react-bootstrap'
-
 import React from 'react'
 
-const Product = ({product}) => {
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+import Rating from './Rating'
+
+const Product = ({ product }) => {
   return (
     <Card>
-        <a href={`/product/${product._id}`}>
-            <Card.Img src='{product.image' variant='top'></Card.Img>
-        </a>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top'></Card.Img>
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div" className='product-title'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
+
+        <Card.Text as="h3">
+          ${product.price}
+        </Card.Text>
+
+      </Card.Body>
     </Card>
   )
 }
