943ab983358411bb2c7772fc6b03d3c2804dc2d8
Berkay Yılmaz <berkayyilmaz@Berkay-MacBook-Air.local>
2024-09-10 03:58:57 +0300

    Seeding and get products from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 const PORT = process.env.PORT || 5050;
 
 connectDB(); //Connect to MongoDB
@@ -13,16 +13,7 @@ app.get('/', (req, res) =>{
     res.send('API is running...');
 });
 
-//Get all products
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
-
-//Get the product with by id
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
