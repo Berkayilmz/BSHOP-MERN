fd464fe0908e870b564bc93c1153fbb5b804e1c4
Berkay Yılmaz <berkayyilmaz@Berkay-MacBook-Air.local>
2024-09-10 15:11:15 +0300

    Custom error middleware and product controller added

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js';
 const PORT = process.env.PORT || 5050;
 
 connectDB(); //Connect to MongoDB
 
-const app = express(); 
+const app = express();
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
